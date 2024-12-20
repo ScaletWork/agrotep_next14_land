2b0ea5bc82a9e0ee6ea3ecde56909e9f545c4b60
Maxim <maximpalaguta@gmail.com>
2024-12-20 09:09:53 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,15 @@
 const nextConfig = {
   images: { unoptimized: true },
 
-  // For Build version
+  // For Build version github
 
   // output: "export",
   // basePath: "/Agrotep",
   // assetPrefix: "/Agrotep/",
+
+  // For Build version normal host
+
+  output: "export",
 };
 
 export default nextConfig;
